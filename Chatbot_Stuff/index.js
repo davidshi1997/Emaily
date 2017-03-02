526933390c7534d5eda6d686d973b95f435a1fb8
davidshi1997 <davidshi1997@gmail.com>
2017-03-02 17:35:43 -0500

    Update index.js

--- Chatbot_Stuff/index.js
+++ Chatbot_Stuff/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var mongodb = require('mongodb')
+const crypto = require('crypto');
 var app = express()
 var promise = require('promise')
 var Imap = require('imap'),
